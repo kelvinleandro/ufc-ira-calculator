497dd6610041189968fdefb92fb39fba5200203d
kelvinleandro <kelvinleandro5@gmail.com>
2025-08-19 16:42:16 -0300

    feat(main): update pdf_path to accept CLI argument or dynamic input

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import sys
 from pathlib import Path
 from src.pdf_parser import extract_disciplines
 from src.calculations import calculate_individual_ira, calculate_general_ira
 
 
 def main():
+    try:
+        pdf_path = Path(sys.argv[1])
+    except IndexError:
+        pdf_path = Path(input("Digite o caminho para o arquivo PDF: "))
+
     MEDIA_CURSO = 7.2652
     DESVIO_CURSO = 1.8389
 
-    pdf_path = Path("data/historico.pdf")
     disciplinas = extract_disciplines(pdf_path)
     ira_i = calculate_individual_ira(disciplinas)
     ira_g = calculate_general_ira(ira_i, MEDIA_CURSO, DESVIO_CURSO)
